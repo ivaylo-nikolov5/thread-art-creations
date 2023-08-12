14706a84c3ab5571790bf6c7d30f3b77b538cabe
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-08-12 19:02:08 +0300

    Added the categories component

--- src/components/HomePage/CategoriesChoice.jsx
+++ src/components/HomePage/CategoriesChoice.jsx
@@ -7,21 +7,62 @@ function CategoriesChoice() {
 
     return (
         <div className="categories-slider">
-            <div className="category-container">
-                <img src={images[0]} className="category-img" alt="" />
-                <label>Мъже</label>
+            <div>
+                <div className="category-container">
+                    <img src={images[0]} className="category-img" alt="" />
+                    <div className="category-label-container">
+                        <label className="category-label">Men</label>
+                    </div>
+                    
+                </div>
+
+                <p className="items-link">See the items 
+                        <span></span>
+                </p>
+
             </div>
-            <div className="category-container">
-                <img src={images[1]} className="category-img" alt="" />
-                <label>Жени</label>
+
+            <div>
+                <div className="category-container">
+                    <img src={images[1]} className="category-img" alt="" />
+                    <div className="category-label-container">
+                        <label className="category-label">Women</label>
+                    </div>
+                    
+                </div>
+
+                <p className="items-link">See the items 
+                        <span></span>
+                </p>
             </div>
-            <div className="category-container">
-                <img src={images[2]} className="category-img" alt="" />
-                <label>Деца</label>
+
+            <div>
+                <div className="category-container">
+                    <img src={images[2]} className="category-img" alt="" />
+                    <div className="category-label-container">
+                        <label className="category-label">Kids</label>
+                    </div>
+                    
+                </div>
+
+                <p className="items-link">See the items 
+                        <span></span>
+                </p>
             </div>
-            <div className="category-container">
-                <img src={images[3]} className="category-img" alt="" />
-                <label>Аксесоари</label>
+
+            <div>
+                <div className="category-container">
+                    <img src={images[3]} className="category-img" alt="" />
+                    <div className="category-label-container">
+                        <label className="category-label">Accessories</label>
+                    </div>
+                    
+                </div>
+
+                <p className="items-link">See the items 
+                        <span></span>
+                </p>
+
             </div>
         </div>
     );
