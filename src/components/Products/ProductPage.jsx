288c0c6dfefb9faeb11da04d6d66c04b384788ce
ivaylo-nikolov5 <www.ivailo135@gmail.com>
2023-08-31 16:42:58 +0300

    Initial commit

--- src/components/Products/ProductPage.jsx
+++ src/components/Products/ProductPage.jsx
@@ -4,6 +4,7 @@ import getUrlData from './data/getUrlData';
 import fetchData from './data/fetchData';
 import "../../styles/ProductsStyles/product-page.css";
 import Path from '../small_components/Path';
+import ProductImages from '../small_components/ProductImages';
 
 
 const ProductPage = (props) => {
@@ -21,6 +22,10 @@ const ProductPage = (props) => {
             <Header />
             <div className='path-container'>
                 <Path items={breadcrumbItems}/>
+            </div>
+
+            <div>
+                <ProductImages />
             </div>
         </div>
     );
---
+++ src/components/small_components/ProductImages.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ProductImages = () => {
+    return (
+        <div className='product-images-container'>
+            
+        </div>
+    )
+}
+
+export default ProductImages
